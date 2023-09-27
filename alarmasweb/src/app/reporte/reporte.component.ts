e5f42f579072aafbbf2598a0593bb908461c16af
s-cortes <s.cortes@uniandes.edu.co>
2023-09-26 23:10:56 -0500

    feat(web): reporte

--- alarmasweb/src/app/reporte/reporte.component.ts
+++ alarmasweb/src/app/reporte/reporte.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Alarma } from '../alarma';
 
 @Component({
   selector: 'app-reporte',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./reporte.component.css']
 })
 export class ReporteComponent {
+  alarma: Alarma = { 
+    id: 1, 
+    title: "GYM Lunes", 
+    location: "../../assets/map_detalle.png", 
+    fecha: "18/09/23", 
+    hora: "06:00" 
+  }
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
