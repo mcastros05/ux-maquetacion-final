5c4892f719c11ed8215dfcd2dab74e8032d00e5b
Melissa Castro <m.castros@uniandes.edu.co>
2023-09-27 20:37:33 -0500

    añadir alarma y seetings activity

---
+++ alarmasmovil/app/src/main/java/co/edu/uniandes/misw4302/alarmasmovil/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.uniandes.misw4302.alarmasmovil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
