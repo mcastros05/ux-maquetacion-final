ee27dc982b6db21cc0749795b00230da78827e1b
s-cortes <s.cortes@uniandes.edu.co>
2023-09-27 21:45:23 -0500

    feat(movil): navegacion

--- alarmasmovil/app/src/main/java/co/edu/uniandes/misw4302/alarmasmovil/MainActivity.kt
+++ alarmasmovil/app/src/main/java/co/edu/uniandes/misw4302/alarmasmovil/MainActivity.kt
@@ -1,11 +1,28 @@
 package co.edu.uniandes.misw4302.alarmasmovil
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnGoogle = findViewById<Button>(R.id.button_google)
+        var btnAndroid = findViewById<Button>(R.id.button_android)
+
+        btnGoogle.setOnClickListener {
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnAndroid.setOnClickListener {
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
